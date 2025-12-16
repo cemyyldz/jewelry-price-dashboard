7efdc4772a5e621d9bf59d6da4a64536bb02b898
Oğulcem Yıldız <91990302+cemyyldz@users.noreply.github.com>
2025-12-16 21:19:31 +0300

    updates

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -45,40 +45,6 @@ function intToDateString(i, get_saat) {
 
 }
 
-function _isnumber(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-}
-
-function _formatNumber(number) {
-    if (isNaN(number)) return "0";
-
-    var temp = number.toString().replace(",", ".");
-    var tmp = temp.split(".");
-
-
-    let tamKisim = tmp[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
-
-    if (typeof tmp[1] != "undefined" && tmp[1] !== "") {
-        let ondalik = tmp[1].substring(0, 2); 
-        return tamKisim + "," + ondalik;
-    } else {
-        return tamKisim;
-    }
-}
-
-
-function ekranaSayiFormatla(s, yuvarla) {
-
-    if (yuvarla == undefined) {
-        yuvarla = 3;
-    }
-    let parts = parseFloat(s).toFixed(yuvarla).toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    return parts.join(",");
-}
-
-
 $(window).on("resize",function(e){
     if ( !is_mobile && $(window).width()<900) {
         window.location.href="?size=500"

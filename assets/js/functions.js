cc6c83dca792b078a7150898a572cad3581e15f8
Oğulcem Yıldız <91990302+cemyyldz@users.noreply.github.com>
2025-12-16 21:16:37 +0300

    upp

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -45,30 +45,6 @@ function intToDateString(i, get_saat) {
 
 }
 
-function intToDateString2(i, get_saat) {
-    let d = new Date(i);
-    let g = d.getDate();
-    let a = d.getMonth() + 1;
-    let saat = d.getHours();
-    let dk = d.getMinutes();
-    let sn = d.getSeconds();
-
-    if (g < 10) g = '0' + g;
-    if (a < 10) a = '0' + a;
-    if (saat < 10) saat = '0' + saat;
-    if (dk < 10) dk = '0' + dk;
-    if (sn < 10) sn = '0' + sn;
-    if (get_saat == true) {
-        return saat + ':' + dk + ':' + sn;
-    }
-    else {
-        return d.getFullYear() + '-' + a + '-' + g + ' ' + saat + ':' + dk + ':' + sn;
-    }
-
-}
-
-
-
 function _isnumber(num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
